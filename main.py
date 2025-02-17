63bd1e13792ce97b88732ea12fc1d246d6abd92b
Vladislav-Subbotin <vlad23082003@gmail.com>
2025-02-17 15:14:48 +0300

    добавил защиту от неверных значений

---
+++ main.py
@@ -0,0 +1,64 @@
+import random
+class warrior:
+  def __init__(self):
+    print('введите количество здоровия воина')
+    try:
+      self.health = int(input())
+    except ValueError:
+        print('это не число')
+        self.health = 0
+    print('введите количество брони воина')
+    try:
+      self.armor = int(input())
+    except ValueError:
+        print('это не число')
+        self.armor = 0
+    print('введите количество выносливости воина')
+    try:
+      self.stamina = int(input())
+    except ValueError:
+        print('это не число')
+        self.stamina = 0
+  def hithit(self, enemy):
+    #print('оба ударили')
+    if self.stamina > 0:
+      self.stamina-=10
+      enemy.health-=random.randint(10, 30)
+    else:
+      enemy.health-=random.randint(0, 10)
+    if enemy.stamina > 0:
+      enemy.stamina-=10
+      self.health-=random.randint(10, 30)
+    else:
+      self.health-=random.randint(0, 10)
+  def hitdefense(self, enemy):
+    #print('1 ударил другого')
+    if self.stamina > 0:
+      self.stamina-=10
+      if enemy.armor > 0:
+        enemy.health-=random.randint(0, 20)
+        enemy.armor-=random.randint(0, 10)
+      else:
+        enemy.health-=random.randint(10, 30)
+    else:
+      enemy.health-=random.randint(0, 10)
+unit1 = warrior()
+unit2 = warrior()
+while unit1.health > 10 and unit2.health > 10:
+  #print(unit1.health,' - ',unit2.health)
+  #print(unit1.armor,' - ',unit2.armor)
+  #print(unit1.stamina,' - ',unit2.stamina)
+  #print('\n')
+  r1 = random.randint(1, 2)
+  r2 = random.randint(1, 2)
+  if r1 == 1 and r2 == 1:
+    unit1.hithit(unit2)
+  elif r1 == 1 and r2 == 2:
+    unit1.hitdefense(unit2)
+  elif r1 == 2 and r2 == 1:
+    unit2.hitdefense(unit1)
+else:
+  if unit1.health <= 10:
+    print('победил 2 воин')
+  else:
+    print('победил 1 воин')
